5b5c1bf06d74cbb1821c105aaddc24d178e1065c
Dmitriy <dmitriyweb@inbox.ru>
2023-09-22 11:20:21 +0500

    add custom hook useDebounse

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -4,10 +4,7 @@ function useDebounce<T>(value: T, delay?: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value)
   
   useEffect(() => {
-    //create a timer to delay setting the value.
     const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
-
-    //if the value changes, we clear the timeout and do not change the value
     return () => {
       clearTimeout(timer);
     };
